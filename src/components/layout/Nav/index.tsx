a691c218adaba910423a46f77c6fa143fd822f93
zakirhossen23 <73053667+zakirhossen23@users.noreply.github.com>
2022-05-31 04:19:58 +0600

    fixed 2

--- src/components/layout/Nav/index.tsx
+++ src/components/layout/Nav/index.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 // import {ContentLoader} from '@/components/common/ContentLoader'
 import { useIntl } from 'react-intl'
 import { NavLink } from 'react-router-dom'
-import { Button, Grid, makeStyles } from "@material-ui/core";
 import './index.scss'
 import Router from 'next/router'
 
